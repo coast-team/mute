69f695351cc5319624cacb65442d32b630f6d105
Philippe Kalitine <philippe.kalitine@gmail.com>
2016-11-02 10:30:56 +0100

    chore(css): add normilize.css and material icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { HttpModule } from '@angular/http'
+import { MaterialModule } from '@angular/material'
 
 import { AppComponent } from './app.component'
 import { EditorComponent } from './editor/editor.component'
@@ -16,7 +17,8 @@ import { CoreModule } from './core/core.module'
     BrowserModule,
     CoreModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
