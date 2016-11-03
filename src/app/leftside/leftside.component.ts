a6e13bb9df045779ef18f8b0f140c628fe0095a2
Philippe Kalitine <philippe.kalitine@gmail.com>
2016-11-03 18:19:07 +0100

    feat(docTitle): add input for document title

--- src/app/leftside/leftside.component.ts
+++ src/app/leftside/leftside.component.ts
@@ -5,7 +5,7 @@ import { ProfileService } from '../core/profile/profile.service'
 @Component({
   selector: 'mute-leftside',
   templateUrl: './leftside.component.html',
-  styleUrls: ['./leftside.component.css'],
+  styleUrls: ['./leftside.component.scss'],
   providers: [ProfileService]
 })
 export class LeftsideComponent implements OnInit {
