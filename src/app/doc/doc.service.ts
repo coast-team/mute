08f932342786e391d01316d64f94a78a9f5bfad2
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2016-11-15 13:08:52 +0100

    feat(doc): Apply remote operations and stream resulting text operations
    Subscribe to the stream of remote LogootSplit operations, apply them to the data model and generate
    
    a new stream of the resulting text operations

--- src/app/doc/doc.service.ts
+++ src/app/doc/doc.service.ts
@@ -1,21 +1,33 @@
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
 
+import { NetworkService } from '../core/network/network.service'
+
 import * as MuteStructs  from 'mute-structs'
 
 @Injectable()
 export class DocService {
 
   private doc: any
+  private network: NetworkService
+  private remoteTextOperationsStream: Observable<any[]>
 
-  constructor() {
+  constructor(network: NetworkService) {
     this.doc = new MuteStructs.LogootSRopes(0)
+    this.network = network
+    this.remoteTextOperationsStream = this.network.onRemoteOperations.map( (logootSOperation: any) => {
+      return this.handleRemoteOperation(logootSOperation)
+    })
   }
 
   setTextOperationsStream(textOperationsStream: Observable<any[]>) {
     textOperationsStream.subscribe( (array: any[][]) => {
       this.handleTextOperations(array)
     })
+  }
+
+  getRemoteTextOperationsStream(): Observable<any[]> {
+    return this.remoteTextOperationsStream
   }
 
   handleTextOperations(array: any[][]) {
@@ -26,6 +38,11 @@ export class DocService {
       })
       console.log('doc: ', this.doc)
     })
+  }
+
+  handleRemoteOperation(logootSOperation: any) {
+    const textOperations: any[] = logootSOperation.execute(this.doc)
+    return textOperations
   }
 
 }
