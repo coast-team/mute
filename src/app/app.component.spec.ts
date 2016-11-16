7e69a7ba50c1e1e1f9e7df30e57bf62b5117f37c
Philippe Kalitine <philippe.kalitine@gmail.com>
2016-11-16 14:31:28 +0100

    build(angular-cli): update angular-cli to 1.0.0-beta.20-4

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,7 +3,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('App: Mute', () => {
+describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,10 @@ export class AppComponent {
   public ghdeploy: boolean
 
   constructor () {
+    /*
+     FIXME: Hide devlable for development. Leave it only got github deploy.
+     Check: https://github.com/angular/angular-cli#build-targets-and-environment-files
+     */
     this.ghdeploy = environment.ghdeploy
   }
 }
