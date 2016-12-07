8455e3be7adbd67b36a5b42686a19d3fbb06a5a5
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2016-12-07 16:27:15 +0100

    refactor(collaborators): Clean CollaboratorsComponent

--- src/app/doc/rightside/collaborators/collaborators.component.ts
+++ src/app/doc/rightside/collaborators/collaborators.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 
 import { CollaboratorsService, Collaborator } from '../../../core/collaborators'
 
@@ -9,42 +9,13 @@ import { CollaboratorsService, Collaborator } from '../../../core/collaborators'
 })
 export class CollaboratorsComponent implements OnInit {
 
-  private changeDetectorRef: ChangeDetectorRef
   private collabService: CollaboratorsService
 
   public collaborators: Array<Collaborator>
 
-  // TODO: look at changeDetectorRef. Maybe is not a very efficient way to detect changes
-  constructor(collabService: CollaboratorsService, changeDetectorRef: ChangeDetectorRef) {
-    this.changeDetectorRef = changeDetectorRef
+  constructor(collabService: CollaboratorsService) {
     this.collabService = collabService
-    this.collaborators = new Array<Collaborator>()
   }
 
-  ngOnInit() {
-    this.collabService.onJoin.subscribe(({id, pseudo, color}: {id: number, pseudo: string, color: string}) => {
-      this.collaborators.push(new Collaborator(id, pseudo, color))
-      this.changeDetectorRef.detectChanges()
-    })
-
-    this.collabService.onLeave.subscribe((id: number) => {
-      for (let i = 0; i < this.collaborators.length; i++) {
-        if (this.collaborators[i].id === id) {
-          this.collaborators.splice(i, 1)
-          break
-        }
-      }
-      this.changeDetectorRef.detectChanges()
-    })
-
-    this.collabService.onPseudoChange.subscribe(({id, pseudo}: {id: number, pseudo: string}) => {
-      for (let i = 0; i < this.collaborators.length; i++) {
-        if (this.collaborators[i].id === id) {
-          this.collaborators[i].pseudo = pseudo
-          break
-        }
-      }
-      this.changeDetectorRef.detectChanges()
-    })
-  }
+  ngOnInit() {}
 }
