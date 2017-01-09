c0787f4b3ac742279c0c06cc2b932ad1cd7d6f9b
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2017-01-09 18:19:30 +0100

    feat(botStorage): Edit BotStorageService to extend AbstractStorageService

--- src/app/core/bot-storage/bot-storage.service.spec.ts
+++ src/app/core/bot-storage/bot-storage.service.spec.ts
@@ -1,16 +1,16 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
-import { BotStorageService } from './bot-storage.service';
+import { TestBed, async, inject } from '@angular/core/testing'
+import { BotStorageService } from './bot-storage.service'
 
 describe('BotStorageService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [BotStorageService]
-    });
-  });
+    })
+  })
 
   it('should ...', inject([BotStorageService], (service: BotStorageService) => {
-    expect(service).toBeTruthy();
-  }));
-});
+    expect(service).toBeTruthy()
+  }))
+})
--- src/app/core/bot-storage/bot-storage.service.ts
+++ src/app/core/bot-storage/bot-storage.service.ts
@@ -1,27 +1,38 @@
 import { Injectable } from '@angular/core'
 import { Http } from '@angular/http'
 
+import { AbstractStorageService } from '../AbstractStorageService'
+
 import { environment } from '../../../environments/environment'
 
 @Injectable()
-export class BotStorageService {
+export class BotStorageService extends AbstractStorageService {
 
+  readonly name: string = 'Remote Bot Storage'
   public currentBot: {url: string, key: string} = {url: '', key: ''}
 
-  constructor(private http: Http) {}
+  constructor(private http: Http) {
+    super()
+  }
 
-  reachable (): Promise<void> {
+  isReachable (): Promise<boolean> {
     return this.http.get(`http://${environment.botStorageAPI}/ping`).toPromise()
       .then((response) => {
         if (typeof response.text() !== 'string' || response.text() !== 'pong') {
           let msg = 'Wrong bot storage response on /ping'
           log.error(msg)
-          throw new Error(msg)
+          return false
+          // FIXME: Re-enable the following line?
+          // throw new Error(msg)
         }
+        return true
+      })
+      .catch(() => {
+        return false
       })
   }
 
-  getDocuments (): Promise<void> {
+  getDocuments (): Promise<any> {
     return this.http.get(`http://${environment.botStorageAPI}/docs`).toPromise()
       .then((response) => {
         log.debug('DOCS: ', response.json())
