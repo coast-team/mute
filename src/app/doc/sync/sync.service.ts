3b83e558f80f70bb5d021126f94c043266d3822c
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2017-01-17 13:42:17 +0100

    feat(sync): Extract and emit LogootSOperations from RichLogootSplitOperations

--- src/app/doc/sync/sync.service.ts
+++ src/app/doc/sync/sync.service.ts
@@ -13,14 +13,26 @@ export class SyncService {
   private localRichLogootSOperationObservable: Observable<RichLogootSOperation>
   private localRichLogootSOperationObserver: Observer<RichLogootSOperation>
 
+  private remoteLogootSOperationObservable: Observable<LogootSAdd | LogootSDel>
+  private remoteLogootSOperationObserver: Observer<LogootSAdd | LogootSDel>
+
   constructor () {
     this.localRichLogootSOperationObservable = Observable.create((observer) => {
       this.localRichLogootSOperationObserver = observer
     })
+
+    this.remoteLogootSOperationObservable = Observable.create((observer) => {
+      this.remoteLogootSOperationObserver = observer
+    })
+
   }
 
   get onLocalRichLogootSOperation (): Observable<RichLogootSOperation> {
     return this.localRichLogootSOperationObservable
+  }
+
+  get onRemoteLogootSOperation (): Observable<LogootSAdd | LogootSDel> {
+    return this.remoteLogootSOperationObservable
   }
 
   set localLogootSOperationSource (source: Observable<LogootSAdd | LogootSDel>) {
@@ -30,6 +42,12 @@ export class SyncService {
       this.localRichLogootSOperationObserver.next(richLogootSOp)
 
       this.clock++
+    })
+  }
+
+  set remoteRichLogootSOperationSource (source: Observable<RichLogootSOperation>) {
+    source.subscribe((richLogootSOp: RichLogootSOperation) => {
+      this.remoteLogootSOperationObserver.next(richLogootSOp.logootSOp)
     })
   }
 
