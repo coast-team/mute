a2e072fcedfc60bf0d1adc731e513b74a89d4a2f
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2017-01-20 11:08:11 +0100

    feat(sync): Add function fromPlain() to RichLogootSOperation

--- src/app/doc/sync/RichLogootSOperation.ts
+++ src/app/doc/sync/RichLogootSOperation.ts
@@ -6,6 +6,21 @@ export class RichLogootSOperation {
   readonly clock: number
   readonly logootSOp: LogootSAdd | LogootSDel
 
+  static fromPlain (o: {id: number, clock: number, logootSOp: any}): RichLogootSOperation | null {
+
+    const logootSAdd: LogootSAdd | null = LogootSAdd.fromPlain(o.logootSOp)
+    if (logootSAdd instanceof LogootSAdd) {
+      return new RichLogootSOperation(o.id, o.clock, logootSAdd)
+    }
+
+    const logootSDel: LogootSDel | null = LogootSDel.fromPlain(o.logootSOp)
+    if (logootSDel instanceof LogootSDel) {
+      return new RichLogootSOperation(o.id, o.clock, logootSDel)
+    }
+
+    return null
+  }
+
   constructor (id: number, clock: number, logootSOp: LogootSAdd | LogootSDel) {
     this.id = id
     this.clock = clock
