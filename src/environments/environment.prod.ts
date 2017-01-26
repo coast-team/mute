d985bec35ccbdae4a9a0e435c22eb7ca346a04c5
Philippe Kalitine <philippe.kalitine@gmail.com>
2017-01-26 10:21:53 +0100

    fix(signaling): move to openshift signaling for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   devLabel: true,
-  signalingURL: 'http://signal2.loria.fr',
+  signalingURL: 'wss://sigver8-coastteam.rhcloud.com:8000',
   botStorageAPI: 'localhost:8080',
   botStorage: 'localhost:9000'
 }
