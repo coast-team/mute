223a55a62bb0853a701b734c1bb917f539e7faf4
Philippe Kalitine <philippe.kalitine@gmail.com>
2017-01-26 17:10:36 +0100

    fix(signaling): correct signaling port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   devLabel: true,
-  signalingURL: 'wss://sigver8-coastteam.rhcloud.com:8000',
+  signalingURL: 'wss://sigver8-coastteam.rhcloud.com:8443',
   botStorageAPI: 'localhost:8080',
   botStorage: 'localhost:9000'
 }
