7bd9b57c182a8d9522383e75335ff8cf61e2175e
Philippe Kalitine <philippe.kalitine@gmail.com>
2017-01-27 15:43:43 +0100

    refactor(docs): DocsModule no longer depends on NavModule

--- src/app/docs/docs.module.ts
+++ src/app/docs/docs.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
 
 import { SharedModule } from 'shared'
-import { NavModule } from 'nav'
 import { DocsComponent } from './docs.component'
 
 @NgModule({
@@ -11,7 +10,6 @@ import { DocsComponent } from './docs.component'
   imports: [
     CommonModule,
     SharedModule,
-    NavModule,
     RouterModule.forChild([
       {path: '', component: DocsComponent}
     ])
