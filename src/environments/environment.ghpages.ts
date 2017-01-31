7f31e6b918aacfd1f7461a21bb984e77f8e9211b
Philippe Kalitine <philippe.kalitine@gmail.com>
2017-01-31 18:36:49 +0100

    chore(ghpages): add build env for ghpages

---
+++ src/environments/environment.ghpages.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  devLabel: true,
+  signalingURL: 'wss://sigver8-coastteam.rhcloud.com:8443',
+  botStorageAPI: 'localhost:8080',
+  botStorage: 'localhost:9000'
+}
