13958faf38ddacf37d54c544ecf06ca3d2d7be2c
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2017-02-13 13:17:15 +0100

    fix(cursor): Edit directive to use NetworkService.send() instead of newSend()

--- src/app/doc/editor/cursor/cursors.directive.ts
+++ src/app/doc/editor/cursor/cursors.directive.ts
@@ -63,12 +63,12 @@ export class CursorsDirective extends ServiceIdentifier implements OnChanges, On
         this.pbPosition.setBaseList(cursor.base)
         this.pbCursor.setPosition(this.pbPosition)
       }
-      this.network.newSend(this.constructor.name, this.pbCursor.serializeBinary())
+      this.network.send(this.constructor.name, this.pbCursor.serializeBinary())
     }
 
     CodeMirror.on(this.cmEditor, 'blur', () => {
       this.pbCursor.setVisible(false)
-      this.network.newSend(this.constructor.name, this.pbCursor.serializeBinary())
+      this.network.send(this.constructor.name, this.pbCursor.serializeBinary())
       CodeMirror.off(cmDoc, 'cursorActivity', updateCursor)
     })
 
