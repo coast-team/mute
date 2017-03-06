6e1f8efa494bf7aa2f09a8127916d2af12bb4cda
Philippe Kalitine <philippe.kalitine@gmail.com>
2017-03-06 10:06:23 +0100

    chore(flexlayout): update package to v2.0.0-rc.1 and fix mute-core version

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -41,19 +41,19 @@
 
 
 /** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
+import 'core-js/es6/reflect'
+import 'core-js/es7/reflect'
 
 
 /** ALL Firefox browsers require the following to support `@angular/animation`. **/
-import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+import 'web-animations-js'  // Run `npm install --save web-animations-js`.
 
 
 
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js/dist/zone'  // Included with Angular CLI.
 
 
 
