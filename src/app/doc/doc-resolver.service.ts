0e1caf28973627df0e2a5e6d3eef1c06dda60c59
Philippe Kalitine <philippe.kalitine@gmail.com>
2017-03-06 14:24:39 +0100

    fix(doc): url resolver for .../doc/MYKEY

--- src/app/doc/doc-resolver.service.ts
+++ src/app/doc/doc-resolver.service.ts
@@ -8,7 +8,7 @@ import { File } from '../core/File'
 import { Doc } from '../core/Doc'
 
 @Injectable()
-export class DocResolverService implements Resolve<string> {
+export class DocResolverService implements Resolve<Doc> {
 
   constructor (
     private router: Router,
@@ -16,46 +16,32 @@ export class DocResolverService implements Resolve<string> {
     private localStorage: LocalStorageService
   ) {}
 
-  resolve (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<string> {
+  resolve (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<Doc> {
     const urlKey = route.params['key']
-    return new Promise((resolve, reject) => {
-      const activeFile = this.ui.activeFile
-      if (!activeFile.isDoc()) {
-        log.warn('Edit a document, but the active file is not a document, thus navigate to home: ', activeFile)
-        this.router.navigate([''])
+    let activeFile = this.ui.activeFile as Doc
+    if (activeFile && activeFile instanceof Doc) {
+      if (urlKey === activeFile.id) {
+        return this.localStorage.get(activeFile.id)
+          .then((localDoc: Doc) => {
+            if (localDoc === null) {
+              activeFile.save()
+            }
+            return activeFile
+          })
       } else {
-        let doc = activeFile as Doc
-        if (urlKey === doc.id) {
-          this.localStorage.get(doc.id)
-            .then((localDoc: Doc) => {
-              if (localDoc === null) {
-                localDoc.save()
-              }
-              resolve(doc)
-            })
-        } else {
-          this.localStorage.get(urlKey)
-            .then((localDoc: Doc) => {
-              if (localDoc === null) {
-                doc = this.localStorage.createDoc(urlKey)
-                this.ui.setActiveFile(doc)
-              } else {
-                this.ui.setActiveFile(doc)
-              }
-              resolve(doc)
-            })
-        }
+        return this.localStorage.get(urlKey)
+          .then((localDoc: Doc) => {
+            if (localDoc === null) {
+              activeFile = this.localStorage.createDoc(urlKey)
+            }
+            this.ui.setActiveFile(activeFile)
+            return activeFile
+          })
       }
-    })
-
-    // let id = route.params['id']
-    // return this.cs.getCrisis(id).then(crisis => {
-    //   if (crisis) {
-    //     return crisis;
-    //   } else { // id not found
-    //     this.router.navigate(['/crisis-center']);
-    //     return null;
-    //   }
-    // });
+    } else {
+      activeFile = this.localStorage.createDoc(urlKey)
+      this.ui.setActiveFile(activeFile)
+      return Promise.resolve(activeFile)
+    }
   }
 }
