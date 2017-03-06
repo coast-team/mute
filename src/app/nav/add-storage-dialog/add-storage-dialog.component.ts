23974f345a2edbd0ba9d343142df60d451abc2e7
Philippe Kalitine <philippe.kalitine@gmail.com>
2017-03-06 10:46:32 +0100

    build(nav): fix build by commenting add-storage-dialog

--- src/app/nav/add-storage-dialog/add-storage-dialog.component.ts
+++ src/app/nav/add-storage-dialog/add-storage-dialog.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core'
-import { MdDialogRef } from '@angular/material'
+// import { Component } from '@angular/core'
+// import { MdDialogRef } from '@angular/material'
 
-@Component({
-  selector: 'mute-add-storage-dialog',
-  templateUrl: './add-storage-dialog.component.html'
-})
-export class AddStorageDialogComponent {
+// @Component({
+//   selector: 'mute-add-storage-dialog',
+//   templateUrl: './add-storage-dialog.component.html'
+// })
+// export class AddStorageDialogComponent {
 
-  constructor (
-    public dialogRef: MdDialogRef<AddStorageDialogComponent>
-  ) { }
-}
+//   constructor (
+//     public dialogRef: MdDialogRef<AddStorageDialogComponent>
+//   ) { }
+// }
