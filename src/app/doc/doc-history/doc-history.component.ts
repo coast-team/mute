adf337f54e751957d673086872ab04cb229826a4
Philippe Kalitine <philippe.kalitine@gmail.com>
2017-04-07 10:11:51 +0200

    fix(doc-history): initialize editor outside of Angular zone

--- src/app/doc/doc-history/doc-history.component.ts
+++ src/app/doc/doc-history/doc-history.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, Injectable, Input, ViewChild } from '@angular/core'
+import {
+  Component,
+  OnInit,
+  Injectable,
+  Input,
+  ViewChild,
+  ElementRef,
+  NgZone } from '@angular/core'
 import { DocService } from 'mute-core/lib'
 import * as CodeMirror from 'codemirror'
 
@@ -19,11 +26,13 @@ export class DocHistoryComponent implements OnInit {
   private isInited = false
 
   @Input() docService: DocService
-  @ViewChild('editorElt') editorElt
+  @ViewChild('editorElt') editorElt: ElementRef
 
   public editor: CodeMirror.Editor
 
-  constructor () { }
+  constructor (
+    private zone: NgZone
+  ) { }
 
   countVersions (): number {
     // return this.versions.length
@@ -31,11 +40,33 @@ export class DocHistoryComponent implements OnInit {
   }
 
   ngOnInit () {
+    const elm1 = document.getElementById('textArea')
+    const elm2 = this.editorElt.nativeElement
+    /*
+    * Here you have elm1 === elm2.
+    * The first is a native browser approach.
+    * The second is an Angular approach.
+    * But the result is the same.
+    */
 
-    this.editor = CodeMirror(document.getElementById('textArea'), {
-      value: 'this is a sample text',
-      mode: 'gfm',
-      readOnly: 'true'
+    /*
+    * We have NgZone imported in this module and injected its instance
+    * by Angular (see constructor property).
+    * We run the following code what we call outside of Angular zone,
+    * because we do not want Angular detect any modification done inside
+    * CodeMirror and manage it ourselves.
+    * Q. Why this?
+    * A. To understand well a more detailed comprehension of Angular
+    * detect changes mechanism is mandatory, but in two words
+    * if we do not do it, we will have a performance issue,
+    * as Angular would run detectChanges mechanism infinitely.
+    */
+    this.zone.runOutsideAngular(() => {
+      this.editor = CodeMirror(elm1, {
+        value: 'this is a sample text',
+        mode: 'gfm',
+        readOnly: 'true'
+      })
     })
   }
 }
