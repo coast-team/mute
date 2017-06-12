8655668f737f551494ecec5e4fdd524efc09abff
Quentin Tardivon <quentin.tardivon@gmail.com>
2017-06-12 13:42:32 +0200

    refactor(progressive): SW register in independant class and emit event for trigering notifications

--- src/app/docs/docs.component.ts
+++ src/app/docs/docs.component.ts
@@ -8,6 +8,7 @@ import { LocalStorageService, BotStorageService } from '../core/storage'
 import { Folder } from '../core/Folder'
 import { File } from '../core/File'
 import { Doc } from '../core/Doc'
+import { ServiceWorkerRegister } from '../core/ServiceWorkerRegister'
 import { UiService } from '../core/ui/ui.service'
 
 @Component({
@@ -23,6 +24,7 @@ export class DocsComponent implements OnDestroy, OnInit {
   private activeFileSubs: Subscription
   private mediaSubscription: Subscription
   private activeMediaQuery: string
+  private serviceWorker: ServiceWorkerRegister
 
   private snackBarSubject: Subject<string>
   private activeFolder: Folder
@@ -38,11 +40,12 @@ export class DocsComponent implements OnDestroy, OnInit {
     private botStorage: BotStorageService,
     public ui: UiService,
     private ref: ChangeDetectorRef,
-    public media: ObservableMedia
+    public media: ObservableMedia,
   ) {
     this.snackBarSubject = new Subject()
     this.docs = []
     this.visibleBtns = []
+    this.serviceWorker = new ServiceWorkerRegister
   }
 
   ngOnInit () {
@@ -81,55 +84,12 @@ export class DocsComponent implements OnDestroy, OnInit {
           duration: 5000
         })
       })
+    
+    this.serviceWorker.registerSW()
 
-    const parent = this
-
-    if ('serviceWorker' in navigator) {
-      // Delay registration until after the page has loaded, to ensure that our
-      // precaching requests don't degrade the first visit experience.
-      // See https://developers.google.com/web/fundamentals/instant-and-offline/service-worker/registration
-      window.addEventListener('load', function () {
-        // Your service-worker.js *must* be located at the top-level directory relative to your site.
-        // It won't be able to control pages unless it's located at the same level or higher than them.
-        // *Don't* register service worker file in, e.g., a scripts/ sub-directory!
-        // See https://github.com/slightlyoff/ServiceWorker/issues/468
-        navigator.serviceWorker.register('service-worker.js').then(function (reg) {
-          // updatefound is fired if service-worker.js changes.
-          reg.onupdatefound = function () {
-            // The updatefound event implies that reg.installing is set; see
-            // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
-            let installingWorker = reg.installing
-
-
-            installingWorker.onstatechange = function () {
-              switch (installingWorker.state) {
-              case 'installed':
-                if (navigator.serviceWorker.controller) {
-                    // At this point, the old content will have been purged and the fresh content will
-                    // have been added to the cache.
-                    // It's the perfect time to display a "New content is available; please refresh."
-                    // message in the page's interface.
-                  parent.snackBarSubject.next('Updated application is available, refresh your browser!')
-                  console.log('New or updated content is available.')
-                } else {
-                    // At this point, everything has been precached.
-                    // It's the perfect time to display a "Content is cached for offline use." message.
-                  parent.snackBarSubject.next('Content available offline')
-                  console.log('Content is now available offline!')
-                }
-                break
-              case 'redundant':
-                console.error('The installing service worker became redundant.')
-                break
-              }
-            }
-          }
-        })
-        .catch(function (e) {
-          console.error('Error during service worker registration:', e)
-        })
-      })
-    }
+    this.serviceWorker.observableState.subscribe(message => {
+      this.snackBarSubject.next(message)
+    })
 
 
   }
