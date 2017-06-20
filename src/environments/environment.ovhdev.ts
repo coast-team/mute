90f95473aba250cd1aba05c2a432a846c32b9df6
Philippe Kalitine <philippe.kalitine@gmail.com>
2017-06-20 13:43:49 +0200

    build: fix env files, add ovhdev env var

---
+++ src/environments/environment.ovhdev.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  devLabel: true,
+  fetchIceServers: true,
+  signalingURL: 'wss://www.coedit.re:20443',
+  storages: [
+    {secure: true, host: 'www.coedit.re', port: 21443}
+  ]
+}
