b8941c77cf6ea8b33144b2e6d8ddb95536e1d511
Camille <camille.menou@telecomnancy.net>
2017-07-06 14:41:36 +0200

    feat(editor): Keyboard shortcuts work on several lines. Using links is more user-friendly

--- src/app/doc/editor/editor.service.ts
+++ src/app/doc/editor/editor.service.ts
@@ -65,12 +65,13 @@ export class EditorService {
     this.editor.addKeyMap({
       // bold
       'Ctrl-B': (cm) => {
-        this.toggleStyle(cm, '**', {'**': new RegExp('.*\\*\\*.*\\*\\*.*'), __: new RegExp('.*__.*__.*')})
+        this.toggleStyle(cm, '**', {'**': new RegExp('[\\s\\S]*\\*\\*[\\s\\S]*\\*\\*[\\s\\S]*'),
+          __: new RegExp('[\\s\\S]*__[\\s\\S]*__[\\s\\S]*')})
       },
       // italic
       'Ctrl-I': (cm) => {
-        this.toggleStyle(cm, '_', {_: new RegExp('[^_]*(_|___)[^_].*[^_](_|___)[^_]*', 'y'),
-          '*': new RegExp('[^\\*]*(\\*|\\*\\*\\*)[^\\*].*[^\\*](\\*|\\*\\*\\*)[^\\*]*', 'y')})
+        this.toggleStyle(cm, '_', {_: new RegExp('[^_]*(_|___)[^_][\\s\\S]*[^_](_|___)[^_]*', 'y'),
+          '*': new RegExp('[^\\*]*(\\*|\\*\\*\\*)[^\\*][\\s\\S]*[^\\*](\\*|\\*\\*\\*)[^\\*]*', 'y')})
       },
       // strikethrough
       'Ctrl-Alt-S': (cm) => {
@@ -78,17 +79,38 @@ export class EditorService {
       },
       // link
       'Ctrl-K': (cm) => {
-        let s = cm.getSelection()
-        let t = s.slice(0, 1) === '[' && s.slice(-6) === ['](url)']
-        cm.replaceSelection(t ? s.slice(1, -6) : '[' + s + '](url)', 'around')
+        this.handleLink(cm)
       }
     })
   }
 
+  handleLink (cm: any): void {
+    const selectedText = cm.getSelection()
+    const linkRE = new RegExp('^\\[[\\s\\S]*\\]\\([\\s\\S]*\\)', 'y')
+    let pos = cm.listSelections()[0].anchor
+    let line = pos.line
+    let ch = pos.ch
+
+    if (!linkRE.test(selectedText)) {
+      cm.replaceSelection('[' + selectedText + '](url)', 'end')
+      pos = cm.getCursor()
+      line = pos.line
+      ch = pos.ch
+      cm.setCursor({line, ch: ch - 4})
+    } else {
+      let sizeOfLinkBeforeURL = selectedText.match(new RegExp('\\[[\\s\\S]*\\]\\('))[0].length
+      if (ch < cm.listSelections()[0].head.ch) {
+        cm.setCursor({line, ch: ch + sizeOfLinkBeforeURL})
+      } else {
+        cm.setCursor({line, ch: ch - (selectedText.length - sizeOfLinkBeforeURL)})
+      }
+    }
+  }
+
   toggleStyle (cm: any, tokenSyntax: string, reTokenSyntaxs: any): void {
     let isAlreadyStyled = false
-
     const selectedText = cm.getSelection()
+
     if (selectedText.length > 0) {
       for (let reTokenSyntax in reTokenSyntaxs) {
         if (typeof reTokenSyntaxs[reTokenSyntax] === typeof(new RegExp(''))) {
