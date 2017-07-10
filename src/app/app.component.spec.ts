fb98166d476b89f70c1fffb51c62bd1b6e24bda7
Quentin Tardivon <quentin.tardivon@gmail.com>
2017-07-10 15:00:11 +0200

    test(app): Create a simple test for the app component with Angular TestBed

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,43 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { RouterTestingModule } from '@angular/router/testing'
+import { Component } from '@angular/core'
+
+import { AppComponent } from './app.component'
+
+import { RouterOutletStubComponent } from '../testing/router-stubs'
+
+@Component({selector: 'mute-toolbar', template: ''})
+class ToolbarStubComponent {}
+
+@Component({selector: 'mute-dev-label', template: ''})
+class DevLabelStubComponent {}
+
+let comp: AppComponent
+let fixture: ComponentFixture<AppComponent>
+
+describe('AppComponent & Testmodule', () => {
+  beforeEach(async (() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [
+        AppComponent,
+        ToolbarStubComponent,
+        DevLabelStubComponent,
+        RouterOutletStubComponent
+      ]
+    })
+
+    .compileComponents()
+    .then(() => {
+      fixture = TestBed.createComponent(AppComponent)
+      comp = fixture.componentInstance
+    })
+  }))
+  tests()
+})
+
+function tests () {
+  it('Correct Init', () => {
+    expect(comp).toBeTruthy()
+  })
+}
---
+++ src/testing/router-stubs.ts
@@ -0,0 +1,10 @@
+import { Component, Directive, Injectable, Input } from '@angular/core'
+import { NavigationExtras } from '@angular/router'
+
+@Component({selector: 'router-outlet', template: ''})
+export class RouterOutletStubComponent { }
+
+@Injectable()
+export class RouterStub {
+  navigate (commands: any[], extras?: NavigationExtras) {}
+}
