02f12eb1156dc795b74b74a23da845f43de07197
Philippe Kalitine <philippe.kalitine@gmail.com>
2018-01-12 11:36:35 +0100

    fix(auth): add missing baseUrl parameter

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -9,24 +9,10 @@ import { BotStorageService } from './storage/bot-storage/bot-storage.service'
 import { StorageService } from './storage/storage.service'
 import { UiService } from './ui/ui.service'
 
-// export class AuthConfig extends CustomConfig {
-
-//   constructor () {
-//     super()
-//     if (environment.auth.baseUrl !== '') {
-//       this.baseUrl = environment.auth.baseUrl
-//       this.providers = environment.auth.providers
-//     }
-//     this.defaultHeaders = { 'Content-Type': 'application/json' }
-//   }
-
-// }
-
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule,
-    // Ng2UiAuthModule.forRoot(AuthConfig)
     Ng2UiAuthModule.forRoot(environment.auth)
   ],
   exports: [],
