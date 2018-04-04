45df7a0992bb540b068473eaee93ad69688cc0a1
Philippe Kalitine <philippe.kalitine@gmail.com>
2018-04-04 10:08:29 +0200

    style(lint): fix missing files and enhance lint script

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,11 @@
 import { browser, by, element } from 'protractor'
 
 export class MyProjectPage {
-  navigateTo () {
+  navigateTo() {
     return browser.get('/')
   }
 
-  getParagraphText () {
+  getParagraphText() {
     return element(by.css('app-root h1')).getText()
   }
 }
