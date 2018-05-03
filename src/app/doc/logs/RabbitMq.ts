9cd58e7fc6eabdac07827da273ccbcb8b975eca2
Cedric Enclos <cedric.enclos@loria.fr>
2018-05-03 17:43:38 +0200

    feat(rabbitmq): Send offline-logs which are stored in the local storage when offline

--- src/app/doc/logs/RabbitMq.ts
+++ src/app/doc/logs/RabbitMq.ts
@@ -1,28 +1,69 @@
-import { StompConfig, StompService } from '@stomp/ng2-stompjs'
+import { StompConfig, StompRService, StompService, StompState } from '@stomp/ng2-stompjs'
+import { Message } from '@stomp/stompjs'
+import { Observable } from 'rxjs/Observable'
+import { Subject } from 'rxjs/Subject'
 import { Database } from './Database'
 
-export class RabbitMq {
+export class RabbitMq extends StompService {
 
-  private stompService: StompService
   private queue: string
   private key: string
 
-  constructor (docKey: string) {
-    this.queue = '/queue/muteLogs'
-    this.key = docKey
+  private queueLength: number
+  private localName: string
 
+  constructor (docKey: string) {
     const config = new StompConfig()
     config.url = 'ws://localhost:15674/ws'
     config.headers = { login: 'guest', passcode: 'guest' }
-    config.debug = false
+    config.debug = true
     config.heartbeat_in = 0
     config.heartbeat_out = 20000
     config.reconnect_delay = 5000
-    this.stompService = new StompService(config)
+    super(config)
+
+    this.queue = '/queue/muteLogs'
+    this.key = docKey
+    this.queueLength = this.queuedMessages.length
+    this.localName = 'offline-logs-' + this.key
+
+    if (window.localStorage.getItem(this.localName) === null) {
+      // The local storage is empty, we initialize the offline save with an empty array
+      window.localStorage.setItem(this.localName, JSON.stringify([]))
+    } else {
+      // The localstorage have some logs, we add them into the queue
+      const oldLogs = JSON.parse(window.localStorage.getItem(this.localName)) as string[]
+      oldLogs.forEach((log, key) => {
+        this.queuedMessages.push({ queueName: this.queue, message: log, headers: this.client.ws.headers })
+      })
+    }
   }
 
   public send (data: object): void {
-    const obj = { collection: this.key, data }
-    this.stompService.publish(this.queue, JSON.stringify(obj))
+    const obj = JSON.stringify({ collection: this.key, data })
+    this.publish(this.queue, obj)
+
+    if (this.queuedMessages.length > this.queueLength) {
+      console.log('ADDDDDD to localstorage')
+      const oldLogs = JSON.parse(window.localStorage.getItem(this.localName)) as string[]
+      oldLogs.push(obj)
+      window.localStorage.setItem(this.localName, JSON.stringify(oldLogs))
+      this.queueLength = this.queuedMessages.length
+    }
+  }
+
+  protected sendQueuedMessages (): void {
+    const queuedMessages = this.queuedMessages
+    this.queuedMessages = []
+    this.queueLength = 0
+    window.localStorage.setItem(this.localName, JSON.stringify([]))
+
+    this.debug(`Will try sending queued messages ${queuedMessages}`)
+
+    for (const queuedMessage of queuedMessages) {
+      this.debug(`Attempting to send ${queuedMessage}`)
+      this.publish(queuedMessage.queueName, queuedMessage.message, queuedMessage.headers)
+    }
+
   }
 }
