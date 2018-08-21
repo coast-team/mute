48184bb331e3f3e89f842005d2e9dc0dcbf2d866
Philippe Kalitine <philippe.kalitine@gmail.com>
2018-08-21 14:40:23 +0200

    fix(date): show the format correctly

--- src/app/shared/pipes/dateEnhanced.pipe.ts
+++ src/app/shared/pipes/dateEnhanced.pipe.ts
@@ -1,27 +1,31 @@
 import { DatePipe } from '@angular/common'
 import { Pipe, PipeTransform } from '@angular/core'
 
-const today = new Date()
-const todayYear = today.getFullYear()
-const todayDate = today.getDate()
+const oneDay = 864e5 // 864e5 == 86400000 == 24*60*60*100
 
 @Pipe({
   name: 'muteDate',
 })
 export class DateEnhancedPipe extends DatePipe implements PipeTransform {
   transform(date: any): string {
+    const today = new Date()
+    const todayYear = today.getFullYear()
+    today.setUTCHours(0, 0, 0, 0)
     if (date && date instanceof Date) {
       let prefix = ''
-      let format = 'MMMM d, y, h:mm a'
-
-      if (todayYear === date.getFullYear()) {
-        format = 'MMMM d, h:mm a'
-        if (todayDate === date.getDate()) {
-          format = 'h:mm a'
-        } else if (todayDate === date.getDate() - 1) {
+      let format: string
+      const diff = today.getTime() - date.getTime()
+      if (diff > 0) {
+        if (diff < oneDay) {
+          format = 'HH:mm'
           prefix = 'yesterday '
+        } else {
+          format = todayYear === date.getFullYear() ? 'MMM d, y' : 'MMM d'
         }
+      } else {
+        format = 'HH:mm'
       }
+
       return prefix + super.transform(date, format)
     }
     return ''
