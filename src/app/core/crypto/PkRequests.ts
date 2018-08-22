87f9b3f389c3b59044bac32a3d746d36aef00527
Philippe Kalitine <philippe.kalitine@gmail.com>
2018-08-22 16:14:33 +0200

    fix(coniks): should work without coniksClient env parameter

--- src/app/core/crypto/PkRequests.ts
+++ src/app/core/crypto/PkRequests.ts
@@ -2,21 +2,30 @@ import { HttpClient } from '@angular/common/http'
 import { environment } from '../../../environments/environment'
 
 export class PkRequests {
-  constructor(private http: HttpClient) {}
+  private url: string
+  constructor(private http: HttpClient) {
+    this.url = 'coniksClient' in environment ? (environment as any).coniksClient.url : ''
+  }
 
   async register(pk: string, login: string) {
     return new Promise((resolve, reject) => {
-      this.http
-        .post(environment.coniksClient.url, `register ${login} ${pk}`, { responseType: 'text' })
-        .subscribe(() => resolve(), (err) => reject(err))
+      if (this.url) {
+        this.http.post(this.url, `register ${login} ${pk}`, { responseType: 'text' }).subscribe(() => resolve(), (err) => reject(err))
+      } else {
+        Promise.reject(new Error('coniksClient property is not defined'))
+      }
     })
   }
 
   async lookup(login: string): Promise<string> {
     return new Promise((resolve, reject) => {
-      this.http
-        .post(environment.coniksClient.url, `lookup ${login}`, { responseType: 'text' })
-        .subscribe((pk) => resolve(pk.match(/\{.*\}/)[0]), (err) => reject(err))
+      if (this.url) {
+        this.http
+          .post(this.url, `lookup ${login}`, { responseType: 'text' })
+          .subscribe((pk) => resolve(pk.match(/\{.*\}/)[0]), (err) => reject(err))
+      } else {
+        Promise.reject(new Error('coniksClient property is not defined'))
+      }
     }) as Promise<string>
   }
 }
