d63f635f1c1f30879ec734ddf25a059e9c84e6cb
Rigel Kent <sendmemail@rigelk.eu>
2021-03-02 11:18:31 +0100

    refactor(release): decouple archive signing from build

--- .release-it.js
+++ .release-it.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   "hooks": {
     "before:bump": "scripts/bump.sh ${version}",
-    "after:bump": "scripts/archive-sign.sh ${name} ${version}",
+    "after:bump": "npm run build && scripts/archive-sign.sh ${name} ${version}",
     "after:release": "echo Successfully released ${name} v${version} to ${repo.repository}."
   },
   "disable-metrics": true
