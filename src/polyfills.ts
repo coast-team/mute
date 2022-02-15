a013d8a09d03dd2f56fb24ec9377f88cc0429c1b
Baptiste Hubert <baptiste.hubert@inria.fr>
2022-02-15 14:49:56 +0100

    feat: updating the project to use ESLint instead of TSLint

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,4 +1,4 @@
-/* tslint:disable */
+/* eslint-disable */
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
